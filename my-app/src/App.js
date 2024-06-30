d25ad3e4e6389ee7bd2fa63e7a54ae5d949e8cb1
AnastasiaTshnkva <tsuhankova.anastasia@gmail.com>
2024-06-30 22:55:07 +0300

    added time travel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,56 +1,41 @@
 import './App.css';
 import { useState } from "react";
 
-function App() {
-  function Square({value, onSquareClick}) {
-    return <button className="square" onClick={onSquareClick}>{value}</button>
-  };
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>
+};
 
-  function Board () {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board ({xIsNext, squares, onPlay}) {
+  function handleClick(i) {
+    const nextSquares = squares.slice();
 
-    function handleClick(i) {
-      const newSquares = squares.slice();
-      if(newSquares[i] || calculateWinner(squares)) {
-        return;
-      };
-
-      xIsNext ? newSquares[i] = "X" : newSquares[i] = "O";
-      setSquares(newSquares);
-      setXIsNext(!xIsNext);
+    if(nextSquares[i] || calculateWinner(squares)) {
+      return;
     };
 
-    const winner =calculateWinner(squares);
-    let status;
-    if (winner) {
-      status = "Winner " + winner; 
-    } else {
-      status = "Next player: " + (xIsNext ? "X" : "O");
-    }
-
-    return <>
-    <div className="status">{status}</div>
-    <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
-    </>
+    xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
+    onPlay(nextSquares);
   };
 
-  return <Board/>;
-}
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner " + winner; 
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  const squaresBoard = squares.map((currentSquare, index) => {
+    return <div key={index}>
+      <Square value={squares[index]} onSquareClick={() => handleClick(index)}/>
+      </div>
+  });
+
+  return <>
+    <div className="status">{status}</div>
+    <div>{squaresBoard}</div>
+  </>
+};
 
 function calculateWinner (squares) {
   const lines = [
@@ -74,4 +59,44 @@ function calculateWinner (squares) {
   return null;
 };
 
-export default App;
+export default function Game () {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquare = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  function handlePlay (nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  function jumpToMove (nextMove) {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((square, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to start game";
+    };
+    return (
+      <li key={move}>
+      <button onClick={() => jumpToMove(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+      <ol>{moves}</ol>
+        </div>
+    </div>
+  );
+};
